dc9a1cb8f6f9fadae95a41321586c8ea9d682bc7
Nikita Stepanov <stepanov.nikita95@gmail.com>
2026-01-27 13:47:18 +0100

    Fix CI: use GitHub Secrets for Sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,7 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     sepolia: {
-      url: process.env.ALCHEMY_SEPOLIA_URL,
+      url: process.env.ALCHEMY_SEPOLIA_URL || "https://eth-sepolia.g.alchemy.com/v2/gUXHrINOCqrju1NSCPZmj",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
       chainId: 11155111, 
       gasPrice: "auto"
@@ -21,7 +21,7 @@ const config: HardhatUserConfig = {
     target: "ethers-v6",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY || "your_ETHERSCAN_API_KEY",
+    apiKey: process.env.ETHERSCAN_API_KEY || "D4XDJUTM685W846P2IITKZ3VHSNI9E9PTE",
   },
   sourcify: {
     enabled: true,
